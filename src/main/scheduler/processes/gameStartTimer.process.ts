10cda6e91db104a96d0eda10829c905c10e1804a
jitendra-chauhan <jitendrachauhan527@gmail.com>
2022-04-02 19:42:32 +0530

    minor change

--- src/main/scheduler/processes/gameStartTimer.process.ts
+++ src/main/scheduler/processes/gameStartTimer.process.ts
@@ -6,7 +6,7 @@ class GameStartTimerProcess {
   gameStartTimerProcess = (job: any) => {
     try {
       logger.info("gameStartTimerProcess has call :: ", job.data);
-      // eventEmitter.emit(SCHEDULER.GAME_START_TIMER_SCHEDULE, job.data);
+      eventEmitter.emit(SCHEDULER.GAME_START_TIMER_SCHEDULE, job.data);
     } catch (error) {
       logger.error("gameStartTimerProcess : Error ::", error);
     }
